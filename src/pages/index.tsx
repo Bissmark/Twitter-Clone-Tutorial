4c6173da7daf6e6929d9cac4c2a9f9b6566878d5
CJ Holt <cjholt@CJs-MacBook-Pro.local>
2023-04-03 19:57:44 +1000

    render posts on profile page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,14 @@
 import { SignInButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
-import Head from "next/head";
 
 import { api } from "~/utils/api";
-import type { RouterOutputs } from "~/utils/api";
 
-import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
 import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
 import { toast } from "react-hot-toast";
-import Link from "next/link";
 import { PageLayout } from "~/components/layout";
-
-dayjs.extend(relativeTime);
-
+import { PostView } from "~/components/postview";
 
 const CreatePostWizard = () => {
   const { user } = useUser();
@@ -36,7 +29,7 @@ const CreatePostWizard = () => {
       } else {
         toast.error("Something went wrong");
       }
-    }
+    },
   });
 
   console.log(user);
@@ -44,89 +37,60 @@ const CreatePostWizard = () => {
   if (!user) return null;
 
   return (
-    <div className="flex gap-3 w-full"> 
-      <Image 
-        src={ user.profileImageUrl } 
-        alt={ "Profile Image" } 
+    <div className="flex w-full gap-3">
+      <Image
+        src={user.profileImageUrl}
+        alt={"Profile Image"}
         className="h-14 w-14 rounded-full"
-        width={ 56 }
-        height={ 56 }
+        width={56}
+        height={56}
       />
-      <input 
-        placeholder="Type some emojis!" 
-        className="bg-transparent grow outline-none"
+      <input
+        placeholder="Type some emojis!"
+        className="grow bg-transparent outline-none"
         type="text"
-        value={ input }
-        onChange={ (e) => setInput(e.target.value) }
-        onKeyDown={ (e) => {
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        onKeyDown={(e) => {
           if (e.key === "Enter") {
             e.preventDefault();
             if (input !== "") {
               mutate({ content: input });
             }
-          }}}
-        disabled={ isPosting }
+          }
+        }}
+        disabled={isPosting}
       />
       {input !== "" && !isPosting && (
-        <button onClick={() => mutate({ content: input })} disabled={ isPosting }>
+        <button onClick={() => mutate({ content: input })} disabled={isPosting}>
           Post
         </button>
       )}
 
-      { isPosting && (
-        <div className="flex justfiy-center items-center">
-          <LoadingSpinner size={ 20 } />
+      {isPosting && (
+        <div className="justfiy-center flex items-center">
+          <LoadingSpinner size={20} />
         </div>
       )}
     </div>
-  )
-}
-
-type PostWithUser = RouterOutputs["posts"]["getAll"][number];
-
-const PostView = (props: PostWithUser) => {
-  const { post, author } = props;
-  return (
-    <div key={ post.id } className="flex gap-3 border-b border-slate-400 p-4 ">
-      <Image 
-        src={ author.profileImageUrl } 
-        className="h-14 w-14 rounded-full" 
-        alt={ `@${ author.username}'s profile picture` }
-        width={ 56 }
-        height={ 56 }
-      />
-      <div className="flex flex-col">
-        <div className="flex gap-1 text-slate-300">
-          <Link href={`/@${ author.username }`}>
-            <span>{`@${ author.username } ` }</span>
-          </Link>
-          <Link href={`/post/${ post.id }`}>
-            <span className="font-thin">{` · ${ dayjs(
-              post.createdAt
-            ).fromNow() }` }</span>
-          </Link>
-        </div>
-        <span className="text-2xl">{ post.content }</span>  
-      </div>
-    </div> 
   );
 };
 
 const Feed = () => {
-  const { data, isLoading: postsLoading } = api.posts.getAll.useQuery(); 
+  const { data, isLoading: postsLoading } = api.posts.getAll.useQuery();
 
   if (postsLoading) return <LoadingPage />;
 
-  if (!data) return <div>Something went wrong</div>
+  if (!data) return <div>Something went wrong</div>;
 
   return (
     <div className="flex flex-col">
-      { data.map((fullPost) => (
-        <PostView { ...fullPost } key={ fullPost.post.id } />
+      {data.map((fullPost) => (
+        <PostView {...fullPost} key={fullPost.post.id} />
       ))}
     </div>
-  )
-}
+  );
+};
 
 const Home: NextPage = () => {
   const { isLoaded: userLoaded, isSignedIn } = useUser();
@@ -137,15 +101,15 @@ const Home: NextPage = () => {
   // Return empty div if user isn't loaded
   if (!userLoaded) return <div />;
 
-  return ( 
+  return (
     <PageLayout>
       <div className="flex border-b border-slate-400 p-4">
-        { !isSignedIn && (
+        {!isSignedIn && (
           <div className="flex justify-center">
             <SignInButton />
-          </div> 
+          </div>
         )}
-        { isSignedIn && <CreatePostWizard /> }
+        {isSignedIn && <CreatePostWizard />}
       </div>
       <Feed />
     </PageLayout>
